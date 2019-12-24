2749f76634aecb7d34e4d4644783c9e6d8f188ed
Gaz Jones <gazjones00@hotmail.com>
2019-12-24 16:53:53 +0000

    Add script to setup SQL Server JDBC driver

---
+++ download-sqlserver.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -ex
+
+# sql server jdbc driver
+MSSQL_VERSION=7.4.1
+JRE_VERSION=11
+mvn install:install-file -Dfile=/tmp/mssql-jdbc-${MSSQL_VERSION}.jre${JRE_VERSION}.jar -DgroupId=com.microsoft.sqlserver -DartifactId=mssql-jdbc -Dversion=${MSSQL_VERSION} -Dpackaging=jar
+
+case ${DISTRO} in
+    wildfly*)
+        cat <<-EOF > batch.cli
+batch
+embed-server --std-out=echo
+
+module add --name=com.microsoft.sqlserver --slot=main --resources=/tmp/mssql-jdbc-${MSSQL_VERSION}.jar --dependencies=javax.api,javax.transaction.api
+/subsystem=datasources/jdbc-driver=mssql:add(driver-name="sqlserver",driver-module-name="com.microsoft.sqlserver",driver-xa-datasource-class-name=com.microsoft.sqlserver.jdbc.SQLServerXADataSource)
+
+run-batch
+EOF
+        /camunda/bin/jboss-cli.sh --file=batch.cli
+        ;;
+    *)
+        cp /tmp/mssql-jdbc-${MSSQL_VERSION}.jre${JRE_VERSION}.jar /camunda/lib
+        ;;
+esac
